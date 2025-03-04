b9acb4125b4b9f9067214456ab713fdce813ea94
Josh Little <josh.little@menloschool.org>
2025-03-04 08:19:49 -0800

    Implemented solution we learned in class that utlized the square to the upper left of the current square if two letters currently being checked are equal (which makes sense) and now everything works. Took maybe 10 minutes.

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -23,84 +23,50 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-        System.out.println(alternativeSolution(doc1, doc2));
         // Initialize a "grid" to conduct tabulation by comparing doc1 and doc2.
-        int[][] grid = new int[doc2.length()][doc1.length()];
+        int[][] grid = new int[doc2.length() + 1][doc1.length() + 1];
+
 
         // Create a map that holds the most recent index for each letter in the alphabet.
         map = new int[ALPHABET];
 
         // Iterate through the whole grid and fill using findMaxLength method.
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++)
-                grid[i][j] = findMaxLength(i, j, grid, doc1.charAt(j) == doc2.charAt(i), doc1.charAt(j));
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++)
+                grid[i][j] = findMaxLength(i, j, grid, doc1.charAt(j - 1) == doc2.charAt(i - 1));
 //            increase = false;
         }
 
-        // Prints out formatting
-        System.out.print("[--]");
-        for (int i = 0; i < doc1.length(); i++)
-            System.out.print("[" + doc1.charAt(i) + "]");
-        System.out.println();
-
-        // Prints out grid
-        for (int i = 0; i < grid.length; i++) {
-            System.out.print("[" + doc2.charAt(i) + "]");
-            for (int j = 0; j < grid[0].length; j++)
-                System.out.print("[" + grid[i][j] + "]");
-            System.out.println();
-        }
+//        // Prints out formatting
+//        System.out.print("[--]");
+//        for (int i = 0; i < doc1.length(); i++)
+//            System.out.print("[" + doc1.charAt(i) + "]");
+//        System.out.println();
+//
+//        // Prints out grid
+//        for (int i = 1; i < grid.length; i++) {
+//            System.out.print("[" + doc2.charAt(i - 1) + "]");
+//            for (int j = 0; j < grid[0].length; j++)
+//                System.out.print("[" + grid[i][j] + "]");
+//            System.out.println();
+//        }
 
         // Returns bottom right grid space (aka largest)
-        return grid[doc2.length() - 1][doc1.length() - 1];
+        return grid[doc2.length()][doc1.length()];
 
     }
     // Method to find current longest string of letters that has parameters:
     // current row, collumn, the entire grid, whether chars are equal, and the value of the char.
-    public static int findMaxLength(int row, int col, int[][] grid, boolean equal, int index) {
+    public static int findMaxLength(int row, int col, int[][] grid, boolean equal) {
 
         // Base cases to establish currentLongestString based on left and above neighbors.
         int currentLongest = 0;
-        if (row > 0)
-            currentLongest = grid[row - 1][col];
-        if (col > 0)
-            currentLongest = grid[row][col - 1] > currentLongest ? grid[row][col - 1] : currentLongest;
         // If the two letters are equal, then find more information to know whether to increase longest.
-        if (equal && !increase) {
-            if (map[index] == 0) {
-                currentLongest++;
-                increase = true;
-            }
-            else
-                map[index] = 1;
-        }
+        if (equal)
+            currentLongest = grid[row-1][col-1] + 1;
+        else
+            currentLongest = grid[row][col-1] > grid[row-1][col] ? grid[row][col-1] : grid[row-1][col];
         return currentLongest;
     }
 
-    public static int alternativeSolution(String doc1, String doc2) {
-        int currentIndex = 0;
-        int currentPath = 0;
-
-        // Array that holds a string of numbers separated by commas
-        String[] array = new String[ALPHABET];
-        for (int i = 0; i < array.length; i++)
-            array[i] = "";
-
-        for (int i = 0; i < doc1.length(); i++)
-            array[doc1.charAt(i)] += i + ",";
-
-        // For every instance of a specific letter, find the lowest value and "use" it.
-        for (int i = 0; i < doc2.length(); i++) {
-            List<String> temp = new ArrayList<String>();
-            temp = Arrays.asList(array[doc2.charAt(i)].split(","));
-            for (String s : temp)
-                if (Integer.valueOf(s) > currentIndex) {
-                    currentIndex = Integer.valueOf(s);
-                    currentPath++;
-                    break;
-                }
-        }
-        return currentPath;
-        // Now just implement the heating up solution and it should work.
-    }
 }
