eeebffdfddf0ea0d522755c21c5068cbb83a4e51
Josh Little <josh.little@menloschool.org>
2025-03-06 07:25:52 -0800

    Final Attempt. Comments & Work Log added.

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -7,13 +7,9 @@ import java.util.List;
  * A tool for finding the longest shared substring between two documents.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Josh Little
  */
 public class PlagiarismChecker {
-
-    private static int[] map;
-    private static boolean increase;
-    private static final int ALPHABET = 256;
 
     /**
      * This method finds the longest sequence of characters that appear in both texts in the same order,
@@ -26,45 +22,26 @@ public class PlagiarismChecker {
         // Initialize a "grid" to conduct tabulation by comparing doc1 and doc2.
         int[][] grid = new int[doc2.length() + 1][doc1.length() + 1];
 
-
-        // Create a map that holds the most recent index for each letter in the alphabet.
-        map = new int[ALPHABET];
-
         // Iterate through the whole grid and fill using findMaxLength method.
+        // starts at i=1 and j=1 to have a buffer col/row filled with zeros.
         for (int i = 1; i < grid.length; i++) {
             for (int j = 1; j < grid[0].length; j++)
                 grid[i][j] = findMaxLength(i, j, grid, doc1.charAt(j - 1) == doc2.charAt(i - 1));
-//            increase = false;
         }
 
-//        // Prints out formatting
-//        System.out.print("[--]");
-//        for (int i = 0; i < doc1.length(); i++)
-//            System.out.print("[" + doc1.charAt(i) + "]");
-//        System.out.println();
-//
-//        // Prints out grid
-//        for (int i = 1; i < grid.length; i++) {
-//            System.out.print("[" + doc2.charAt(i - 1) + "]");
-//            for (int j = 0; j < grid[0].length; j++)
-//                System.out.print("[" + grid[i][j] + "]");
-//            System.out.println();
-//        }
-
-        // Returns bottom right grid space (aka largest)
+        // Returns bottom right grid space (the largest)
         return grid[doc2.length()][doc1.length()];
-
     }
-    // Method to find current longest string of letters that has parameters:
-    // current row, collumn, the entire grid, whether chars are equal, and the value of the char.
-    public static int findMaxLength(int row, int col, int[][] grid, boolean equal) {
 
-        // Base cases to establish currentLongestString based on left and above neighbors.
+    // Method to find current longest string of letters that has parameters:
+    // current row, collumn, the entire grid, and whether the two chars being compared are equal.
+    public static int findMaxLength(int row, int col, int[][] grid, boolean equal) {
         int currentLongest = 0;
-        // If the two letters are equal, then find more information to know whether to increase longest.
+        // If the two letters are equal, then take top left square to current grid location and add 1.
         if (equal)
             currentLongest = grid[row-1][col-1] + 1;
         else
+            // otherwise set to the largest between its left neighbor and above neighbor.
             currentLongest = grid[row][col-1] > grid[row-1][col] ? grid[row][col-1] : grid[row-1][col];
         return currentLongest;
     }
